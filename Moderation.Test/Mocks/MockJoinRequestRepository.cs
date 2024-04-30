71fc9062494a852584b14d280b3bbf2c62ba523a
matei cojocariu <mateicojo@yahoo.com>
2024-04-30 17:09:07 +0300

    implemented mocks for joinrequestanswerforonequestiorepo, joinrequestrepo, reportrepo, textpostrepo

---
+++ Moderation.Test/Mocks/MockJoinRequestRepository.cs
@@ -0,0 +1,43 @@
+﻿using Moderation.DbEndpoints;
+using Moderation.Entities;
+using Moderation.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moderation.Test.Mocks
+{
+    internal class MockJoinRequestRepository : JoinRequestRepository
+    {
+        public bool Add(Guid key, JoinRequest value)
+        {
+            return true;
+        }
+
+        public bool Contains(Guid key)
+        {
+            return true;
+        }
+
+        public JoinRequest? Get(Guid key)
+        {
+            return new JoinRequest(Guid.NewGuid());
+        }
+
+        public IEnumerable<JoinRequest> GetAll()
+        {
+            return new List<JoinRequest>() { 
+                new JoinRequest(Guid.NewGuid()),
+                new JoinRequest(Guid.NewGuid()),
+                new JoinRequest(Guid.NewGuid())
+            };
+        }
+
+        public bool Remove(Guid key)
+        {
+            return true;
+        }
+    }
+}
---
+++ Moderation.Test/Mocks/MockTextPostRepository.cs
@@ -0,0 +1,43 @@
+﻿using Moderation.Entities;
+using Moderation.Repository;
+using Moderation.Model;
+
+namespace Moderation.Test.Mocks
+{
+    internal class MockTextPostRepository : TextPostRepository
+    {
+        public bool Add(Guid key, TextPost value)
+        {
+            return true;
+        }
+
+        public bool Remove(Guid key)
+        {
+            return true;
+        }
+
+        public TextPost? Get(Guid key)
+        {
+            return new TextPost("content", new GroupUser(new Guid(), new Guid()));
+        }
+
+        public IEnumerable<TextPost> GetAll()
+        {
+            return new List<TextPost>() { 
+                new TextPost("content1", new GroupUser(new Guid(), new Guid())),
+                new TextPost("content2", new GroupUser(new Guid(), new Guid())),
+                new TextPost("content3", new GroupUser(new Guid(), new Guid()))
+            };
+        }
+
+        public bool Contains(Guid key)
+        {
+            return true;
+        }
+
+        public bool Update(Guid key, TextPost value)
+        {
+            return true;
+        }   
+    }
+}
