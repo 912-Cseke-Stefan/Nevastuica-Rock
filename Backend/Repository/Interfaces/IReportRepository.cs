ba5ed8090e518a1a6d68fc3252ae5ec63e50af7b
Cseke <a@b.com>
2024-04-30 17:03:13 +0300

    Added missing inheritance in a few interfaces

--- Backend/Repository/Interfaces/IReportRepository.cs
+++ Backend/Repository/Interfaces/IReportRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moderation.Model;
 
 namespace Backend.Repository.Interfaces
 {
-    public interface IReportRepository
+    public interface IReportRepository : IRepository<PostReport>
     {
     }
 }
--- Backend/Repository/Interfaces/IRoleRepository.cs
+++ Backend/Repository/Interfaces/IRoleRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moderation.Entities;
 
 namespace Backend.Repository.Interfaces
 {
-    public interface IRoleRepository
+    public interface IRoleRepository : IRepository<Role>
     {
     }
 }
