c3071a777e4ccc1d24187d940571694c55f77b54
912-Crisan-Patricia <patricia.elena.crisan@gmail.com>
2024-04-30 16:47:45 +0300

    MockUserRepositoryAdded

---
+++ Moderation.Test/Mocks/MockUserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Backend.Repository.Interfaces;
+using Moderation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Moderation.Test.Mocks
+{
+    internal class MockUserRepository : IUserRepository
+    {
+        public bool Add(Guid key, User value)
+        {
+            return true;
+        }
+
+        public bool Remove(Guid key)
+        {
+            return true;
+        }
+
+        public User? Get(Guid key)
+        {
+            // Creating a new user with a default constructor
+            return new User(Guid.NewGuid(), "TestUser", "TestPassword");
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            // Creating users with different constructors
+            return new List<User>()
+            {
+                new User("TestUser1"),
+                new User("TestUser2"),
+                new User("TestUser3")
+            };
+        }
+
+        public bool Contains(Guid key)
+        {
+            return true;
+        }
+
+        public bool Update(Guid key, User value)
+        {
+            return true;
+        }
+
+        public Guid? GetGuidByName(string name)
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
