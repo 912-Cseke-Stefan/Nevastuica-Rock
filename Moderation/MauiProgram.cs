6133f4585b4a62a473fc09dcc0b2efa977f338ed
Cseke <stefan.cseke@stud.ubbcluj.ro>
2024-05-02 01:57:45 +0300

    added dependency injection to the project

--- Moderation/MauiProgram.cs
+++ Moderation/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using Moderation.Repository;
+using Backend.Repository.Interfaces;
+using Backend.Repository;
+using Moderation.Serivce;
+using Backend.Service;
 
 namespace Moderation
 {
@@ -15,11 +20,25 @@ namespace Moderation
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddScoped<IGroupRepository, GroupRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddScoped<IAwardRepository, AwardRepository>();
+            builder.Services.AddScoped<IGroupRules, GroupRules>();
+            builder.Services.AddScoped<IGroupUserRepository, GroupUserRepository>();
+            builder.Services.AddScoped<IJoinRequestAnswerForOneQuestionRepository, JoinRequestAnswerForOneQuestionRepository>();
+            builder.Services.AddScoped<IJoinRequestRepository, JoinRequestRepository>();
+            builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+            builder.Services.AddScoped<IReportRepository, ReportRepository>();
+            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+            builder.Services.AddScoped<ITextPostRepository, TextPostRepository>();
+            builder.Services.AddSingleton<ApplicationState>();
+            builder.Services.AddSingleton<Service>();
+            builder.Services.AddSingleton<LoginPage>();
+
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
-
-            builder.Services.AddSingleton<>();
 
             return builder.Build();
         }
