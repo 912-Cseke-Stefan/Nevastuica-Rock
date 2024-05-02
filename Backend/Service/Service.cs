710f5037dca3f4d8342578a5baa8c49e8101cde1
Cseke <stefan.cseke@stud.ubbcluj.ro>
2024-05-02 10:53:15 +0300

    removed Service capability of storing its own instance

--- Backend/Service/Service.cs
+++ Backend/Service/Service.cs
@@ -6,18 +6,11 @@ namespace Backend.Service
 {
     public class Service
     {
-        private static Service instance;
         private ApplicationState state;
 
         public Service(ApplicationState state)
         {
             this.state = state;
-            instance = this;
-        }
-
-        public static Service GetService()
-        {
-            return instance;
         }
 
         public GroupUser GetGroupUserFromPostReport(PostReport report)
@@ -33,6 +26,26 @@ namespace Backend.Service
         public User GetUserByGuid(Guid id)
         {
             return state.UserRepository.Get(id);
+        }
+
+        public IEnumerable<Group> GetAllGroups()
+        {
+            return state.Groups.GetAll();
+        }
+
+        public List<TextPost> GetPostsOfAuthorsInGivenGroup(Group group)
+        {
+            return state.TextPosts.GetAll().Where(post => post.Author.GroupId == group.Id).ToList();
+        }
+
+        public IEnumerable<PostReport> GetReportsWhichBelongToGivenGroup(Group group)
+        {
+            return state.Reports.GetAll().Where(report => report.GroupId == group.Id);
+        }
+
+        public IEnumerable<JoinRequest> GetJoinRequestsForGivenGroup(Group group)
+        {
+            return state.JoinRequests.GetAll().Where(request => state.GroupUsers.Get(request.UserId)?.GroupId == group.Id);
         }
     }
 }
